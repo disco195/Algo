8f9742c6c77e257dd85df0fbb634a85cf7fed48b
Youssef AbouEgla <youssefegla@gmail.com>
2020-05-28 22:34:27 +0200

    Action Creators for sorting algorithms

---
+++ src/actions/sort.ts
@@ -0,0 +1,28 @@
+import { randomSet } from "../common/functions";
+import {
+  SORT_ACTIONS as ACTIONS,
+  SORT_CREATE_OPTIONS as OPTIONS,
+} from "../store/actionTypes";
+
+export function createSet(
+  option: OPTIONS,
+  payload?: number[]
+): { type: string; payload: number[] } {
+  const type: ACTIONS = "CREATE_SET";
+
+  switch (option) {
+    case "RANDOM":
+      return { type, payload: randomSet() };
+
+    case "USER_DEFINED":
+      if (payload) return { type, payload };
+
+    default:
+      return { type, payload: randomSet() };
+  }
+}
+
+export function sortSet() {
+  const type: ACTIONS = "SORT_SET";
+  return { type };
+}
