fc7d05755a1633b93579d96de4e5951a7b2896ca
Youssef AbouEgla <youssefegla@gmail.com>
2020-05-29 09:52:07 +0200

    Set generation functions 1. randomSet now returns number values only 2. makeSet generates height, width, and key values

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -1,12 +1,30 @@
+import uuid from "uuid";
 /**
  * Generate an array with random length
  * and random set of numbers
  *
  */
-export function randomSet(): number[] {
-  return Array(Math.floor(Math.random() * 35))
+export function randomSet() {
+  // generate random length
+  const length = Math.floor(Math.random() * 35);
+
+  // generate random values
+  return Array(length)
     .fill(null)
-    .map(() => {
-      return Math.floor(Math.random() * 100);
-    });
+    .map(() => Math.floor(Math.random() * 100));
+}
+
+export function makeSet(arr: number[]) {
+  // find maximum value in the set
+  const max = Math.max(...arr);
+
+  // map set and add height, width and key
+  return arr.map((value) => {
+    return {
+      height: (value / max) * 100,
+      width: (1 / arr.length) * 100,
+      value,
+      key: uuid.v4(),
+    };
+  });
 }
