35e8eb673b1ef16b83a1a876c5518c1458a37685
Youssef AbouEgla <youssefegla@gmail.com>
2020-05-31 11:54:06 +0200

    Added Tests for Selection, Insertion and Merge

--- src/store/functions/__tests__/sorting..ts
+++ src/store/functions/__tests__/sorting..ts
@@ -1,4 +1,9 @@
-import { bubbleSort, selectionSort } from "../sorting";
+import {
+  bubbleSort,
+  selectionSort,
+  InsertionSort,
+  mergeSort,
+} from "../sorting";
 
 const Case1 = {
   input: [2],
@@ -48,5 +53,86 @@ describe("Bubble Sort Generator Function", () => {
   it(Case4.message, () => {
     const bubble = bubbleSort(Case4.input);
     expect(bubble.next().value).toEqual(Case4.output);
+  });
+});
+
+/**
+ *
+ * Selection Sort Generator Function
+ *
+ */
+describe("Selection Sort Generator Function", () => {
+  it(Case1.message, () => {
+    const selection = selectionSort(Case1.input);
+    expect(selection.next().value).toEqual(Case1.output);
+  });
+
+  it(Case2.message, () => {
+    const selection = selectionSort(Case2.input);
+    expect(selection.next().value).toEqual(Case2.output);
+  });
+
+  it(Case3.message, () => {
+    const selection = selectionSort(Case3.input);
+    expect(selection.next().value).toEqual(Case3.output);
+  });
+
+  it(Case4.message, () => {
+    const selection = selectionSort(Case4.input);
+    expect(selection.next().value).toEqual(Case4.output);
+  });
+});
+
+/**
+ *
+ * Insertion Sort Generator Function
+ *
+ */
+describe("Insertion Sort Generator Function", () => {
+  it(Case1.message, () => {
+    const insertion = InsertionSort(Case1.input);
+    expect(insertion.next().value).toEqual(Case1.output);
+  });
+
+  it(Case2.message, () => {
+    const insertion = InsertionSort(Case2.input);
+    expect(insertion.next().value).toEqual(Case2.output);
+  });
+
+  it(Case3.message, () => {
+    const insertion = InsertionSort(Case3.input);
+    expect(insertion.next().value).toEqual(Case3.output);
+  });
+
+  it(Case4.message, () => {
+    const insertion = InsertionSort(Case4.input);
+    expect(insertion.next().value).toEqual(Case4.output);
+  });
+});
+
+/**
+ *
+ * Merge Sort Tests
+ *
+ */
+describe("Merge Sort Function", () => {
+  it(Case1.message, () => {
+    const merge = mergeSort(Case1.input);
+    expect(merge).toEqual(Case1.output);
+  });
+
+  it(Case2.message, () => {
+    const merge = mergeSort(Case2.input);
+    expect(merge).toEqual(Case2.output);
+  });
+
+  it(Case3.message, () => {
+    const merge = mergeSort(Case3.input);
+    expect(merge).toEqual(Case3.output);
+  });
+
+  it(Case4.message, () => {
+    const merge = mergeSort(Case4.input);
+    expect(merge).toEqual(Case4.output);
   });
 });
