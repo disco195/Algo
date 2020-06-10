5180bae2820e5911638f87b258ef46145ecd8388
Youssef AbouEgla <youssefegla@gmail.com>
2020-06-10 19:22:50 +0200

    General Reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -5,8 +5,23 @@ const initialState: GeneralState = {
   DS_Type: null,
 };
 
-function generalReducer(state = initialState, action: any) {
-  return state;
+function generalReducer(
+  state = initialState,
+  action: MODULES_ACTION
+): GeneralState {
+  switch (action.type) {
+    case "SORTING":
+      return { ...initialState, currentModule: "SORTING" };
+
+    case "LISTS":
+      return { ...initialState, currentModule: "LISTS" };
+
+    case null:
+      return { ...initialState, currentModule: null };
+
+    default:
+      return state;
+  }
 }
 
 export const reducers = generalReducer;
