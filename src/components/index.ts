0a776561d1c2efa82a68160ed6646e0b9235bb6c
Youssef AbouEgla <youssefegla@gmail.com>
2020-06-11 19:49:37 +0200

    controls main styling

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Navigation } from "./Navigation";
 export { default as NavGroup } from "./NavGroup";
 export { default as Abbr } from "./Abbr";
+export { default as TabPanel } from "./TabPanel";
--- src/containers/Sorting.tsx
+++ src/containers/Sorting.tsx
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 import { Dispatch } from "redux";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { makeStyles, Theme } from "@material-ui/core";
+import { makeStyles, Theme, Tabs, Tab } from "@material-ui/core";
 import { setModule } from "../actions";
+import { TabPanel } from "../components";
 
 interface SortingProps {
   setModule: () => void;
@@ -13,10 +14,43 @@ function Sorting(props: SortingProps) {
   useEffect(() => {
     props.setModule();
   });
-
   const classes = useStyles(props);
 
-  return <main className={classes.root}>Sorting Container</main>;
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <main className={classes.root}>
+      <section className={classes.topContainer}>Visualization</section>
+      <section className={classes.bottomContainer}>
+        <div className={classes.controls}>
+          <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={value}
+            onChange={handleChange}
+            aria-label="Vertical tabs example"
+            className={classes.tabs}
+          >
+            <Tab label="CREATE" />
+            <Tab label="SORT" />
+          </Tabs>
+          <TabPanel value={value} index={0}>
+            <div>Hello</div>
+            <div>Hello</div>
+            <div>Hello</div>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            SORT
+          </TabPanel>
+        </div>
+        <span>code</span>
+      </section>
+    </main>
+  );
 }
 
 //
@@ -26,6 +60,31 @@ const useStyles = makeStyles((theme: Theme) => ({
   root: {
     height: "92vh",
     width: "100%",
+  },
+  topContainer: {
+    height: "65%",
+    width: "80%",
+    margin: "auto",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bottomContainer: {
+    height: "35%",
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+    borderTop: `1px solid ${theme.palette.divider}`,
+  },
+  controls: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: "flex",
+    height: "100%",
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
   },
 }));
 
